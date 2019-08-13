ff3472ea5a5efe93310c7e7e388f9b2b14830fe3
Kasia Kittel <kkittel@equalexperts.com>
2019-08-13 14:46:45 +0100

    Make ec2-generic module cooperating with network module and independent from the region (ami)

--- src/aws_example/modules/aws-ec2-generic/main.tf
+++ src/aws_example/modules/aws-ec2-generic/main.tf
@@ -3,13 +3,40 @@ terraform {
 }
 
 resource "aws_instance" "aws-ec2" {
-  ami           = "ami-025f234ba1c577d83"
+  ami           =  data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   vpc_security_group_ids = var.vpc_sg_id
+  subnet_id = var.subnet_id
+  key_name = var.key_name
 
   tags = {
     Project = "terraform-example-kasia"
     Name = var.aws-ec2-name
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "image-type"
+    values = ["machine"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
   }
 }
--- src/aws_example/modules/aws-ec2-generic/variables.tf
+++ src/aws_example/modules/aws-ec2-generic/variables.tf
@@ -5,7 +5,19 @@ variable "aws-ec2-name" {
 }
 
 variable "vpc_sg_id" {
-  description = "Security group id. Use default security group if not available"
+  description = "Security group id. Use default security group if not set"
   type        = list(string)
+  default     = null
+}
+
+variable "subnet_id" {
+  description = "Subnet id. Use default subnet if not set"
+  type        = string
+  default     = null
+}
+
+variable "key_name" {
+  description = "Key name for SSH session."
+  type        = string
   default     = null
 }
